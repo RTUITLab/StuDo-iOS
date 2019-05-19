1689a3f285dc99bac9b0862d4e3c7055aab1f8cb
Vyacheslav <slava.klevleev@gmail.com>
2019-05-19 22:06:52 +0300

    Started Sign In and Sign Up VC

--- StuDo/AppDelegate.swift
+++ StuDo/AppDelegate.swift
@@ -1,16 +1,24 @@
 import UIKit
 
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        let storyboard = UIStoryboard(name: "Authorization", bundle: nil)
+        
+        let customViewController = storyboard.instantiateViewController(withIdentifier: "CustomViewController") 
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = customViewController
+        window?.makeKeyAndVisible()
+        
         return true
     }
 
-
 }
+
 
