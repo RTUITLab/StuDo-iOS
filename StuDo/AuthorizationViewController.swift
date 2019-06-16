6fedea599ea841d7b638694f989e98addd9f8b1e
Vyacheslav <slava.klevleev@gmail.com>
2019-06-16 15:58:16 +0300

    Other authorization implementation

---
+++ StuDo/AuthorizationViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AuthorizationViewController.swift
+//  StuDo
+//
+//  Created by Вячеслав Клевлеев on 15/06/2019.
+//  Copyright © 2019 Andrew. All rights reserved.
+//
+
+import UIKit
+
+class AuthorizationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
