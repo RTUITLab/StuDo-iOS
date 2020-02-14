cdaf6d421895c816d10f36457f1f39e8163d53ab
Andrew <akw55@icloud.com>
2020-02-15 01:03:48 +0300

    Remove pod libraries from source

--- Pods/Target Support Files/Pods-StuDoTests/Pods-StuDoTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_StuDoTests : NSObject
-@end
-@implementation PodsDummy_Pods_StuDoTests
-@end
--- Pods/Target Support Files/Pods-StuDoUITests/Pods-StuDoUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_StuDoUITests : NSObject
-@end
-@implementation PodsDummy_Pods_StuDoUITests
-@end
